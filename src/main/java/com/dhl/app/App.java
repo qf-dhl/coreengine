5fd959465030498e36319c69f1423387f6db13d7
ravi <ravi@vytec.com>
2023-09-18 15:26:00 +0530

    added

--- src/main/java/com/dhl/app/App.java
+++ src/main/java/com/dhl/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         //System.out.println( "Hello World!" );
-        log.info("hello world");
+        //log.info("hello world");
     }
 }
